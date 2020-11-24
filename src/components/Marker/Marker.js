9046820e5412dc004828171eee90327115fe44e6
Filip FICO <fitz993m@gmail.com>
2020-11-25 00:20:01 +0100

    1.13. Marker component

---
+++ src/components/Marker/Marker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Marker, Popup } from 'react-leaflet';
+import { useSelector } from 'react-redux';
+
+
+const DisplayMarker = () => {
+    const { latitude, longitude, searchObject } = useSelector(state => ({
+        ...state.SearchReducer
+    }))
+
+    const coords = [];
+    coords.push(latitude, longitude);
+
+    return (
+        
+            <Marker position={coords}>
+          <Popup>
+           {
+             searchObject.map(({display_name}) => (
+             <h4>{display_name}</h4>
+             ))
+           }
+          </Popup>
+        </Marker>
+        
+    )
+}
+
+export default DisplayMarker;

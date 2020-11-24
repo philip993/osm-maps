416525aa4f59c45779179aea3e0b38693af8ecc7
Filip Mihajlovski <fitz993m@gmail.com>
2020-11-24 15:53:22 +0100

    1.8. Changes

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import React from "react";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import Search from "../Search/Search";
 
-const Map = () => {
- 
+const NMap = () => {
+  const position = [51.505, -0.09];
+  return (
+    <div>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ width: "500px", height: "500px", margin: "auto" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <div>
+        <Search />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-        <MapContainer center={[51.505, -0.09]} zoom={12} scrollWheelZoom={false}>
-            <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[51.505, -0.09]}></Marker>
-        </MapContainer>
-        </div>
-    )
-}
-
-export default Map;
+export default NMap;

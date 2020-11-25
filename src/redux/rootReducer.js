ef53b29e58664a73a22cd4b8ec3cf2a2334c4388
Filip Mihajlovski <fitz993m@gmail.com>
2020-11-25 15:45:32 +0100

    1.17. Updated reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import { SearchReducer } from "../components/Search/SearchReducer";
+import { RoutingReducer } from "../components/Routing/RoutingReducer";
 
 const rootReducer = combineReducers({
   SearchReducer,
+  RoutingReducer,
 });
 
 export default rootReducer;

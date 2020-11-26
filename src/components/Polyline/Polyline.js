0d40c66118d40e0dea46ca20c74572be21dbd9c8
Filip FICO <fitz993m@gmail.com>
2020-11-27 00:50:10 +0100

    1.26. Added duration/length of routing

--- src/components/Polyline/Polyline.js
+++ src/components/Polyline/Polyline.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { Polyline } from "react-leaflet";
+import { Polyline, Popup } from "react-leaflet";
 import { useSelector } from "react-redux";
 
 const DisplayPolyline = () => {
   const limeOptions = { color: "lime" };
 
-  const { routeDirections } = useSelector((state) => ({
+  const { routeDirections, routeDuration, routeLength } = useSelector((state) => ({
     ...state.RoutingReducer,
   }));
 
-  return <Polyline pathOptions={limeOptions} positions={routeDirections} />;
+  return <Polyline pathOptions={limeOptions} positions={routeDirections}>
+    <Popup>
+      {
+        routeDuration / 60
+      }
+      {
+        routeLength
+      }
+    </Popup>
+  </Polyline>;
 };
 
 export default DisplayPolyline;

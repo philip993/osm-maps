a4eccaccd0b888ec2c00415d1f5d4c6e9b5d145e
Filip FICO <fitz993m@gmail.com>
2020-11-30 22:42:39 +0100

    1.40. Updated

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Search from "../Search/Search";
-import DisplayMap from "../DisplayMap/DisplayMap";
-import DisplayMarker from "../Marker/Marker";
-import Routing from "../Routing/Routing";
-import DisplayPolyline from "../Polyline/Polyline";
-import RouteSearch from "../RouteSearch/RouteSearch";
-import EndMarker from "../EndMarker/EndMarker";
-import StartMarker from "../StartMarker/StartMarker";
-import { getCurrentLocation } from "../RouteSearch/RouteSearchAction";
-import Directions from "../Directions/Directions";
+import DisplayMap from '../DisplayMap/DisplayMap';
+import DisplayPolyline from '../Polyline/Polyline';
+import EndMarker from '../EndMarker/EndMarker';
+import StartMarker from '../StartMarker/StartMarker';
+import Directions from '../Directions/Directions';
+import { getCurrentLocation } from '../RouteSearch/RouteSearchAction';
 
 const NMap = () => {
-  const position = [51.505, -0.09];
   const {
     latitude,
     longitude,
@@ -31,7 +26,7 @@ const NMap = () => {
 
   useEffect(() => {
     dispatch(getCurrentLocation());
-    console.log("map loaded");
+    console.log('map loaded');
   }, []);
 
   console.log(currentLocation);
@@ -39,33 +34,24 @@ const NMap = () => {
   return (
     <div>
       <MapContainer
-        // center={{ lat: latitude, lng: longitude }}
         zoom={13}
         scrollWheelZoom={false}
-        style={{ width: "500px", height: "500px", margin: "auto" }}
+        style={{ width: '500px', height: '500px', margin: 'auto' }}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <DisplayMap />
-        {/* <DisplayMarker /> */}
         <DisplayPolyline />
 
         <div>
           <StartMarker />
-          {latCoord ? <EndMarker /> : "NO END MARKER SELECTED"}
+          {latCoord ? <EndMarker /> : 'NO END MARKER SELECTED'}
         </div>
       </MapContainer>
       <div>
-        <Search />
         <Directions />
-      </div>
-      {/* <div>
-        <RouteSearch />
-      </div> */}
-      <div>
-        <Routing />
       </div>
     </div>
   );

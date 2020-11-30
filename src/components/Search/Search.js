bffa6df97f07049148a389685ba0aaa34e2f63db
Filip FICO <fitz993m@gmail.com>
2020-11-30 22:39:44 +0100

    1.37. Added new form and output layout

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { inputSearchText, requestSearchText } from "./SearchActions";
+import React from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
+import './SearchStyle.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  clearInputSearchText,
+  inputSearchText,
+  requestSearchText,
+} from './SearchActions';
+import Routing from '../Routing/Routing';
 
 const Search = () => {
   const { searchObject, searchText } = useSelector(
-    (state) => state.SearchReducer
+    (state) => state.SearchReducer,
   );
   const dispatch = useDispatch();
 
   const handleSearchInput = (e) => {
     dispatch(inputSearchText(e.target.value));
+  };
+
+  const handleClearSearchInput = () => {
+    dispatch(clearInputSearchText());
   };
 
   const handleSearch = (e) => {
@@ -18,29 +29,48 @@ const Search = () => {
   };
 
   return (
-    <div>
+    <div className="search">
       <h1>Search</h1>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={searchText}
-          onChange={handleSearchInput}
-          placeholder="Search...."
-        />
-        <button type="submit">Search</button>
-      </form>
-      {searchObject.map((s) => (
-        <React.Fragment>
-          <h3>Result</h3>
-          <h4>
-            {s.address.city}, {s.address.country}
-          </h4>
-          <h4>{s.address.postcode}</h4>
-          <h4>
-            Lat: {s.lat} Lng: {s.lon}
-          </h4>
-        </React.Fragment>
-      ))}
+      <Form className="searchForm" onSubmit={handleSearch}>
+        <Form.Label className="formLabel">Search for</Form.Label>
+        <Form.Group className="formGroup">
+          <Form.Control
+            size="lg"
+            required
+            type="text"
+            value={searchText}
+            onChange={handleSearchInput}
+            placeholder="e.g. Barcelona"
+          />
+        </Form.Group>
+        <Form.Group className="formGroupBtn">
+          <Button className="searchBtn" variant="primary" type="submit">
+            Search
+          </Button>
+          <Button
+            className="clearBtn"
+            variant="danger"
+            type="button"
+            onClick={handleClearSearchInput}
+          >
+            Clear
+          </Button>
+        </Form.Group>
+      </Form>
+      {!searchObject
+        ? ''
+        : searchObject.map((s) => (
+            <Card className="card">
+              <Card.Header className="cardHeader">Search Result</Card.Header>
+              <Card.Body className="cardBody">
+                <Card.Title className="cardTitle">{s.display_name}</Card.Title>
+                <Card.Text className="cardText">
+                  Lat: {s.lat} Lng: {s.lng}
+                </Card.Text>
+                <Routing />
+              </Card.Body>
+            </Card>
+          ))}
     </div>
   );
 };

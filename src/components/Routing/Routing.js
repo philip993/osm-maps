d708aadfce5e73cf13ecbfe073058f61f5afe2d0
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-01 15:30:34 +0100

    1.45. Style

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import './RoutingStyle.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { requestCreateRoute, getRouteDirections } from './RoutingActions';
 
 const Routing = () => {
   const { routeObject, routeDirections } = useSelector(
-    (state) => state.RoutingReducer,
+    (state) => state.RoutingReducer
   );
   const dispatch = useDispatch();
   const history = useHistory();

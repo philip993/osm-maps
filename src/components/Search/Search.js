15b03d8038f2653d251aa33be53893f68c86bcc7
Filip FICO <fitz993m@gmail.com>
2020-12-03 01:04:13 +0100

    1.55. Reworked

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, Modal } from 'react-bootstrap';
 import './SearchStyle.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -8,11 +8,13 @@ import {
   requestSearchText,
 } from './SearchActions';
 import Routing from '../Routing/Routing';
+import { toggleModalOff, toggleModalOn } from '../Routing/RoutingActions';
 
 const Search = () => {
-  const { searchObject, searchText } = useSelector(
-    (state) => state.SearchReducer
-  );
+  const { searchObject, searchText, showModal } = useSelector((state) => ({
+    ...state.SearchReducer,
+    ...state.RoutingReducer,
+  }));
   const dispatch = useDispatch();
 
   const handleSearchInput = (e) => {
@@ -21,56 +23,74 @@ const Search = () => {
 
   const handleClearSearchInput = () => {
     dispatch(clearInputSearchText());
+    dispatch(toggleModalOff());
+  };
+
+  const handleModalOff = () => {
+    dispatch(toggleModalOff());
   };
 
   const handleSearch = (e) => {
     e.preventDefault();
     dispatch(requestSearchText());
+    dispatch(toggleModalOn());
   };
 
   return (
     <div className="search">
-      <Form className="searchForm" onSubmit={handleSearch}>
-        <h2>Search</h2>
-        <Form.Group className="formGroup">
-          <Form.Control
-            className="formInput"
-            size="lg"
-            required
-            type="text"
-            value={searchText}
-            onChange={handleSearchInput}
-            placeholder="e.g. Barcelona"
-          />
-        </Form.Group>
-        <Form.Group className="formGroupBtn">
-          <Button className="searchBtn" variant="info" type="submit">
-            Search
-          </Button>
-          <Button
-            className="clearBtn"
-            variant="danger"
-            type="button"
-            onClick={handleClearSearchInput}
-          >
-            Clear
-          </Button>
-        </Form.Group>
+      <Form inline onSubmit={handleSearch} id="f">
+        <Form.Label htmlFor="inlineFormInputName2" srOnly>
+          Search
+        </Form.Label>
+        <Form.Control
+          className="formInput"
+          required
+          type="text"
+          value={searchText}
+          onChange={handleSearchInput}
+          placeholder="e.g. Barcelona"
+        />
+        <Button className="searchBtn" variant="info" type="submit">
+          Search
+        </Button>
       </Form>
-      {!searchObject
-        ? ''
-        : searchObject.map((s) => (
-            <Card className="card">
-              <Card.Header className="cardHeader">Search Result</Card.Header>
-              <Card.Body className="cardBody">
-                <Card.Title className="cardTitle">{s.display_name}</Card.Title>
-                <Card.Text className="cardText">
-                  Lat: {s.lat} Lng: {s.lon}
-                </Card.Text>
-                <Routing />
-              </Card.Body>
-            </Card>
-          ))}
+
+      <Modal
+        show={showModal}
+        onHide={handleModalOff}
+        dialogClassName="modal-90w"
+        // aria-labelledby="example-custom-modal-styling-title"
+        id="modal"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="modalTitle">Search Result</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {!searchObject
+            ? ''
+            : searchObject.map((s) => (
+                <Card className="card">
+                  <Card.Body className="cardBody">
+                    <Card.Title className="cardTitle">
+                      {s.display_name}
+                    </Card.Title>
+                    <Card.Text className="cardText">
+                      Lat: {s.lat} Lng: {s.lon}
+                    </Card.Text>
+                    <Routing />
+                    <Button
+                      className="clearBtn"
+                      variant="danger"
+                      type="button"
+                      onClick={handleClearSearchInput}
+                    >
+                      Clear
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };

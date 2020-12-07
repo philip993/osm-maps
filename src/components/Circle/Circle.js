fe4931336552c6752fd9c29d89c3264393fb1272
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-07 15:44:12 +0100

    2.5. Show all POIs without filter

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { CircleMarker, Marker, Popup } from 'react-leaflet';
 
 const Circle = () => {
-  const { poiPlaces } = useSelector((state) => ({
+  const { poiPlaces, poiSearchCategory } = useSelector((state) => ({
     ...state.POIReducer,
   }));
 
@@ -13,21 +13,27 @@ const Circle = () => {
     <div>
       {!poiPlaces
         ? ''
-        : poiPlaces.map((t, ind) => (
-            // <CircleMarker
-            //   key={ind}
-            //   center={centerCoords}
-            //   radius={10}
-            //   pathOptions={colors}
-            // >
-            <Marker position={t.geometry.coordinates.map((s) => s).reverse()}>
-              <Popup>
-                {ind}.{t.properties.osm_tags.name}
-              </Popup>
-            </Marker>
+        : poiPlaces
+            .filter(
+              poiSearchCategory === ''
+                ? (s) => s
+                : (s) => s.properties.category_ids[poiSearchCategory]
+            )
+            .map((t, ind) => (
+              // <CircleMarker
+              //   key={ind}
+              //   center={centerCoords}
+              //   radius={10}
+              //   pathOptions={colors}
+              // >
+              <Marker position={t.geometry.coordinates.map((s) => s).reverse()}>
+                <Popup>
+                  {ind}.{t.properties.osm_tags.name}
+                </Popup>
+              </Marker>
 
-            // </CircleMarker>
-          ))}
+              // </CircleMarker>
+            ))}
     </div>
   );
 };

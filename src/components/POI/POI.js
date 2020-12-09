8b988afad3fe9f88207ab304bb239ad092320d03
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-09 15:32:25 +0100

    2.7. POI component as Modal in Map

--- src/components/POI/POI.js
+++ src/components/POI/POI.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import './POIStyle.scss';
 import {
-  requestCreatePOISearch,
   selectCategoryFilter,
   clearCategoryFilter,
   toggleOptionsFalse,
-  toggleOptionsTrue,
+  poiModalVisible,
 } from './POIActions';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 const POI = () => {
-  const { poiObject, poiResult } = useSelector((state) => ({
+  const { poiResult } = useSelector((state) => ({
     ...state.POIReducer,
   }));
   const dispatch = useDispatch();
-  const handleSearchPOIs = () => {
-    dispatch(requestCreatePOISearch());
-    dispatch(toggleOptionsTrue());
-  };
+
   const handleGetValue = (e) => {
     dispatch(selectCategoryFilter(e.target.value));
     console.log(e.target.value);
@@ -25,19 +21,50 @@ const POI = () => {
 
   const handleToggleOptionsFalse = () => {
     dispatch(toggleOptionsFalse());
+    dispatch(poiModalVisible());
   };
 
   const handleClearFilter = () => {
     dispatch(clearCategoryFilter());
+    dispatch(toggleOptionsFalse());
   };
   return (
     <div className="pois">
-      <h1>POI</h1>
-      <Button variant="dark" onClick={handleSearchPOIs}>
-        Get POIs
-      </Button>
+      <Modal
+        show={poiResult}
+        onHide={handleToggleOptionsFalse}
+        className="modal"
+      >
+        <Modal.Header closeButton className="mHeader">
+          <Modal.Title className="mTitle">POI Search Filters</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="mBody">
+          <div className="options">
+            <ul onClick={handleGetValue} className="left">
+              <li value={395}>Hairdressers</li>
+              <li value={192}>Bank</li>
+              <li value={191}>ATM</li>
+              <li value={570}>Restaurant</li>
+              <li value={564}>Cafe</li>
+            </ul>{' '}
+            <ul onClick={handleGetValue} className="right">
+              <li value={206}>Hospitals</li>
+              <li value={518}>Supermakerts</li>
+              <li value={298}>Casinos</li>
+              <li value={604}>Station</li>
+              <li value={135}>Churches</li>
+            </ul>
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="mFooter">
+          <Button variant="dark" className="poiBtn" onClick={handleClearFilter}>
+            Show All
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-      <div className="options" hidden={!poiResult}>
+      {/*     NEW PAGE WIHTOUT MODAL  
+      <div className="options" hidden={poiResult}>
         <ul onClick={handleGetValue} className="left">
           <li value={395}>Hairdressers</li>
           <li value={192}>Bank</li>
@@ -53,11 +80,11 @@ const POI = () => {
           <li value={135}>Churches</li>
         </ul>
         <div>
-          <Button variant="dark" onClick={handleClearFilter}>
+          <Button variant="dark" className="poiBtn" onClick={handleClearFilter}>
             Show All
           </Button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
